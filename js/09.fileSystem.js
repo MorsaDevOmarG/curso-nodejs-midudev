342b719637fbf52cf90d74aa5f6dec4b05702f66
Omar Garcia <omar.garcia@morsa.com.mx>
2024-09-22 23:30:02 -0600

    Funciones auto llamadas

---
+++ js/09.fileSystem.js
@@ -0,0 +1,23 @@
+const { readFile } = require('node:fs/promises');
+
+async function init ()  {
+    console.log('Leyendo primer archivo...');
+
+    const text = await readFile('../js/filesSystem.txt', 'utf-8');
+
+    console.log(text);
+
+    console.log('Leyendo contenido, primer archivo...');
+
+    console.log("Haciendo cosas...");
+
+    console.log("Segundo archivo...");
+
+    const secondText = await readFile('../js/fileSystem.txt', 'utf-8');
+
+    console.log(secondText);
+
+    console.log('Leyendo contenido, segundo archivo...');
+}
+
+init();
