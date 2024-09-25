97c9e8fa66ffcdbbf0c8c08a2b097fd693f43e2e
Omar Garcia <omar.garcia@morsa.com.mx>
2024-09-25 01:08:56 -0600

    ENV

--- js/19.variablesEntorno.js
+++ js/19.variablesEntorno.js
@@ -1,11 +1,13 @@
 const http = require('http');
 const { findAvailablePort } = require('./18.terceraAPP');
 
+const port = process.env.PORT ?? 3000;
+
 const server = http.createServer( function (req, res) {
     res.end("Hola Server");
 });
 
-findAvailablePort(3000).then(port => {
+findAvailablePort(port).then(port => {
     server.listen(port, () => {
         console.log("Server listening");
     });
