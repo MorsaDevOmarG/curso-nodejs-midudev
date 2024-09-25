f4cadef34065151ddf7811f4be2bc9cefe8f7765
Omar Garcia <omar.garcia@morsa.com.mx>
2024-09-25 00:50:23 -0600

    Puertos libres

--- js/17.server.js
+++ js/17.server.js
@@ -5,6 +5,12 @@ const server = http.createServer((req, res) => {
     res.end('Hello World!');
 });
 
-server.listen(3000, () => {
-    console.log(`Server listening in port: ${server.port}`);
+// server.listen(3000, () => {
+//     console.log(`Server listening in port: ${server.port}`);
+// });
+
+
+// Utilizar siempre puerto vacío, toma el primer puerto que este disponible
+server.listen(0, () => {
+    console.log(`Server listening in port: ${server.address().port}`);
 });
